7eb35f43054245031508e7cb93092ce20421e2a8
Cibirsk <stlv@gmx.com>
2022-01-07 23:19:00 +0100

    Update Person.php

--- Person.php
+++ Person.php
@@ -6,15 +6,22 @@ class Person
     public $lastName;
     public $age;
 
-    public function __construct($firstName)
+    public function __construct($firstName,$lastName,$age)
     {
         $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->age = $age;
     }
 }
 
-$honore = new Person('Honore');
-$bob = new Person('Bob');
+$honore = new Person('Honore','Balzac',34);
+$bob = new Person('Bob','l\'éponge',45);
+
 echo $honore->firstName . '<br>';
+echo $honore->lastName . '<br>';
+echo $honore->age . '<br>';
 echo $bob->firstName . '<br>';
+echo $bob->lastName . '<br>';
+echo $bob->age . '<br>';
 
 ?>
