61464e14f03a81f34fbbf0ddb2276ce66916c4ce
Cibirsk <stlv@gmx.com>
2022-01-08 12:17:37 +0100

    Update BankAccount.php

--- BankAccount.php
+++ BankAccount.php
@@ -1,7 +1,18 @@
 <?php
 
+class BankAccount
+{
+    public $accountNumber;
 
+    public function __construct($accountNumber)
+    {
+       $this->accountNumber = $accountNumber; 
+    }
+}
 
+$compteBancaireDeHonore = new BankAccount('1234567');
+
+echo $compteBancaireDeHonore->accountNumber . '<br>';
 
 
 ?>
