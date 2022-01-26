07ed525a81228d6acc66a73131de72ec2e1fdfdb
Cibirsk <stlv@gmx.com>
2022-01-26 18:16:55 +0100

    Update Test.php

--- Test.php
+++ Test.php
@@ -16,4 +16,41 @@ class B
     }
 }
 
-(new A)->toto(5);
+class Team
+{
+    private $name;
+    private $nbrFan;
+
+    public function __construct($name,$nbrFan)
+    {
+        $this->name = $name;
+        $this->nbrFan = $nbrFan;
+    }
+
+    public function get_name()
+    {
+        return $this->name;
+    }
+
+    public function get_nbrFan()
+    {
+        return $this->nbrFan;
+    }
+}
+
+class League
+{
+    private $name;
+    private $team;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+        $this->team = [];
+    }
+
+    public function add_team($team)
+    {
+        $this->team[] = $team;
+    }
+}
