0dbc12e16fbbaa02e9b88b34e41591bcc3ad1309
Cibirsk <stlv@gmx.com>
2022-02-07 07:10:10 +0100

    none

--- Test.php
+++ Test.php
@@ -1,15 +1,12 @@
 <?php
 
-require 'src/Person.php';
-require 'src/League.php';
-require 'src/Team.php';
+require 'vendor/autoload.php';
 
+$paris = new Acme\Team('Paris');
+$nantes = new Acme\Team('Nantes');
 
-$paris = new Team('Paris');
-$nantes = new Team('Nantes');
-
-$bob = new Personne('Bob');
-$mike = new Personne('Mike');
+$bob = new Acme\Personne('Bob');
+$mike = new Acme\Personne('Mike');
 $bob->favorite($paris);
 $mike->favorite($paris);
 $mike->favorite($nantes);
@@ -17,7 +14,7 @@ $mike->favorite($nantes);
 echo $paris->get_name() . ' a ' . $paris->get_nbrFan() . ' fans. <br>';
 echo $nantes->get_name() . ' a ' . $nantes->get_nbrFan() . ' fans. <br>';
 
-$ligua = new League('ligua');
+$ligua = new Acme\League('ligua');
 $ligua->add_team($paris);
 $ligua->add_team($nantes);
 echo 'Il y a ' . $ligua->get_teamCount() . ' équipe(s) dans la '
